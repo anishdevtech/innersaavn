ab98d58416d6d6f77f64756d4475d89c9c43f9b7
anishdevtech <anish2dev@gmail.com>
2025-09-18 07:25:48 +0000

    Auto commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,15 @@
-// settings.gradle.kts (root)
 pluginManagement {
-  repositories { google(); mavenCentral(); gradlePluginPortal() }
-  // Option A: Catalog aliases (preferred if you already use libs.versions.toml)
-  // Option B: Direct versions:
-  plugins {
-    id("com.android.application") version "8.7.3"
-    id("com.android.library") version "8.7.3"
-    kotlin("multiplatform") version "2.2.10"
-    kotlin("android") version "2.2.10"
-    kotlin("plugin.serialization") version "2.2.10"
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
   }
 }
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+rootProject.name = "innersaavn"
