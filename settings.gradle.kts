b5033378c5369696d801d00c238a29dcf2649c50
anishdevtech <anish2dev@gmail.com>
2025-09-18 07:29:38 +0000

    Updated Settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,19 @@
 pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    google()
-    mavenCentral()
-  }
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        id("org.jetbrains.kotlin.multiplatform") version "2.0.20"
+        id("com.android.library") version "8.5.2" // or application if you have an app
+    }
 }
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 rootProject.name = "innersaavn"
